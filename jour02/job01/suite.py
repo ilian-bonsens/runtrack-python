9092a38b8e50d74799fe18baafbff5ca8f8a2fe6
ilian-bonsens <ilian.bonsens@laplateforme.io>
2023-11-21 11:12:21 +0100

    job 1, 2, 3 et 4 faits

---
+++ jour02/job01/suite.py
@@ -0,0 +1,3 @@
+for i in range(21):
+    print (i)
+#bien faire attention à l'indentation, print ne marchait pas car pas bien indenté#
---
+++ jour02/job04/tables.py
@@ -0,0 +1,14 @@
+N = int(input("Entrez un entier supérieur à 0 :"))
+for N in range(1,N+1):
+    print(f"""Table de multiplication de {N} :
+    1 x {N} = {1*N}
+    2 x {N} = {2*N}
+    3 x {N} = {3*N}
+    4 x {N} = {4*N}
+    5 x {N} = {5*N}
+    6 x {N} = {6*N}
+    7 x {N} = {7*N}
+    8 x {N} = {8*N}
+    9 x {N} = {9*N}
+    10 x {N} = {10*N}""")
+
