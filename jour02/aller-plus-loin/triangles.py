b8a9dce81a30e2656120b557b5917e6c4ffce74e
ilian-bonsens <ilian.bonsens@laplateforme.io>
2023-11-21 16:21:26 +0100

    pour aller plus loin a finir

---
+++ jour02/aller-plus-loin/triangles.py
@@ -0,0 +1,7 @@
+cote1 = int(input("Entrez une longueur pour le triangle :"))
+cote2 = int(input("Entrez une largeur pour le triangle :"))
+cote3 = int(input("Entrez une hauteur pour le triangle :"))
+
+if cote1 == cote2 or cote1 == cote3 or cote3 == cote2:
+    print("Ceci est un triangle isocèle")
+    if 
