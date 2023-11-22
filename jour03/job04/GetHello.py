bf2993d0d494cafb0c3368dae6d2e47e4320f0af
ilian-bonsens <ilian.bonsens@laplateforme.io>
2023-11-22 11:12:19 +0100

    job 1, 2, 3, 4 et 5 faits

---
+++ jour03/job04/GetHello.py
@@ -0,0 +1,5 @@
+#definition de la fonction GetHello qui va redonner le texte stocké le texte mais pas l'afficher#
+def GetHello():
+    return "Hello la Plateforme."
+print(GetHello())
+#affichage de l'execution de la fonction#
---
+++ jour03/job05/calcul.py
@@ -0,0 +1,11 @@
+def calcul(num1,operator,num2):
+    if operator == "+":
+        print (num1+num2)
+    elif operator == "-":
+        print (num1-num2)
+    elif operator == "*":
+        print (num1*num2)
+    elif operator == "/":
+        print (num1/num2)
+
+calcul(2298,"*",1286)
