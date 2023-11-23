f9cb00962de24eb2ff1d5c0bca9b31799227eee9
ilian-bonsens <ilian.bonsens@laplateforme.io>
2023-11-23 22:19:55 +0100

    à finir

--- jour04/job10/intervalle.py
+++ jour04/job10/intervalle.py
@@ -1,5 +1,6 @@
+L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
+
 def inter():
-    L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
     result = 1 #on créé une variable de valeur 1#
     for num in L:
         if 24 < num < 91:
