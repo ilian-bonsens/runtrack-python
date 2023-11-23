8e1cae1dd817da24c38798abb082b3d0123b11a4
ilian-bonsens <ilian.bonsens@laplateforme.io>
2023-11-23 13:02:55 +0100

    correction job10

--- jour04/job10/intervalle.py
+++ jour04/job10/intervalle.py
@@ -1,5 +1,11 @@
 def inter():
     L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
-    return sum(num for num in L if num > 24 and num < 91)
+    result = 1 #on créé une variable de valeur 1#
+    for num in L:
+        if 24 < num < 91:
+            result = num * result 
+#la boucle va répéter cette opération pour chaque num compris dans cette intervalle et incrémenter le result#
+
+    return result
 
 print(inter())
